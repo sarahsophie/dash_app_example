b6d3eadffcf39bf25c2c5cfdf23309152c66e0f8
Sarah <Sarah@Sarahs-MacBook-Pro-2.local>
2017-12-11 23:17:18 +0100

    2nd version
    2nd version app

--- app.py
+++ app.py
@@ -8,41 +8,49 @@ import pandas as pd
 df = pd.read_csv('nama_10_gdp_1_Data.csv')
 
 
-# In[2]:
+# In[9]:
 
 #Show data
 df.head()
 
 
-# In[ ]:
+# In[10]:
 
-#Define variables
+#Define variables: indicators and countries
 available_indicators=df['NA_ITEM'].unique()
 countries=df['GEO'].unique()
 
 
 # In[ ]:
 
+#Import libraries
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
 
+#Create the app
 app = dash.Dash(__name__)
 server = app.server
 
+#Set CSS style
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+#Design layout of app
 app.layout = html.Div([
 
-    #First graph
-    ### a) scatterplot with two DropDown boxes for the different indicators
-    ### b) slide for the different years in the data
+    #First graph:
+    ### a) Scatterplot with two DropDown boxes for the different indicators
+    ### b) Slider for the different years in the data
     
     html.Div([
-
+        html.H1(children = 'Correlation between indicators'), #Title for graph 1
+        
         html.Div([
+
+            #Create dropdown for x-axis (select indicator)
+            html.H3(children = 'Select x-axis indicator'),
             dcc.Dropdown(
                 id='xaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
@@ -52,6 +60,9 @@ app.layout = html.Div([
         style={'width': '48%', 'display': 'inline-block'}),
 
         html.Div([
+            
+            #Create dropdown for y-axis (select a second indicator)
+            html.H3(children = 'Select y-axis indicator'),
             dcc.Dropdown(
                 id='yaxis-column',
                 options=[{'label': i, 'value': i} for i in available_indicators],
@@ -60,46 +71,55 @@ app.layout = html.Div([
         ],style={'width': '48%', 'float': 'right', 'display': 'inline-block'})
     ]),
 
-    dcc.Graph(id='indicator-graphic'),
-
-    dcc.Slider(
+    dcc.Graph(id='indicator-graphic'), #Unique name for graph 1
+ 
+    #Create slider to select year
+    html.Div(dcc.Slider( 
         id='year--slider',
         min=df['TIME'].min(),
         max=df['TIME'].max(),
         value=df['TIME'].max(),
         step=None,
-        marks={str(year): str(year) for year in df['TIME'].unique()} 
-    ),
- 
-    #Second graph
+        marks={str(time): str(time) for time in df['TIME'].unique()},
+    
+    ), style={'marginRight': 100, 'marginLeft': 100, 'marginTop':50, 'marginBottom':100},),
+    
+    #Second graph:
     ### Line chart
     ### Drop down box for country and indicator
  
     html.Div([
-
+        
+        html.H1(children = 'Development of indicators over time'), #Title for graph 2
+ 
+        #Create dropdown for country / region
         html.Div([
+            html.H3(children = 'Select country / region'),
             dcc.Dropdown(
                 id='series',
                 options=[{'label': i, 'value': i} for i in countries],
                 value='Spain'
             ),
         ],
-        style={'width': '48%', 'marginTop': 40, 'display': 'inline-block'}),
+        style={'width': '48%', 'marginTop': 0, 'display': 'inline-block'}),
 
+        #Create dropdown for y-axis (indicator)
         html.Div([
+            html.H3(children = 'Select y-axis indicator'),
             dcc.Dropdown(
                 id='yaxis-column2',
                 options=[{'label': i, 'value': i} for i in available_indicators],
                 value='Gross domestic product at market prices'
             ),
-        ],style={'width': '48%', 'marginTop': 40, 'float': 'right', 'display': 'inline-block'})
+        ],style={'width': '48%', 'marginTop': 0, 'float': 'right', 'display': 'inline-block'})
     ]),
 
-    dcc.Graph(id='indicator-year-graphic'),
+    dcc.Graph(id='indicator-year-graphic'), #Unique name for graph 2
 
 ])
  
-@app.callback( #first graph
+#Callback for first graph
+@app.callback( 
     dash.dependencies.Output('indicator-graphic', 'figure'),
     [dash.dependencies.Input('xaxis-column', 'value'),
      dash.dependencies.Input('yaxis-column', 'value'),
@@ -127,16 +147,17 @@ def update_graph(xaxis_column_name, yaxis_column_name,
             yaxis={
                 'title': yaxis_column_name,
             },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            margin={'l': 100, 'b': 50, 't': 20, 'r': 100},
             hovermode='closest'
         )
     }
  
-
-@app.callback( #second graph
+#Callback for second graph
+####It is important that variable names do not overlap with callback 1
+@app.callback( 
     dash.dependencies.Output('indicator-year-graphic', 'figure'),
     [dash.dependencies.Input('series', 'value'),
-     dash.dependencies.Input('yaxis-column2', 'value')])
+     dash.dependencies.Input('yaxis-column2', 'value')]) 
 def update_graph(series_name, yaxis_column_name2):
     
     dff = df[df['GEO'] == series_name]
@@ -161,7 +182,7 @@ def update_graph(series_name, yaxis_column_name2):
             yaxis={
                 'title': yaxis_column_name2,
             },
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 0},
+            margin={'l': 100, 'b': 50, 't': 20, 'r': 100},
             hovermode='closest'
         )
     }
@@ -169,5 +190,4 @@ def update_graph(series_name, yaxis_column_name2):
 
 if __name__ == '__main__':
     app.run_server()
-
 
